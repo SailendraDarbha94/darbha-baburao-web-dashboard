c4a6f885269793f2b1389aee08122d101a471af7
SailendraDarbha94 <sailendra.darbha@gmail.com>
2025-09-27 17:48:17 +0530

    barebones nextjs project with tailwind css

---
+++ src/app/test-route/page.tsx
@@ -0,0 +1,7 @@
+export default function TestRoutePage() {
+    return (
+        <main className="flex justify-center items-center h-screen">
+            <h1 className="font-bold text-[40px] text-center">Hello, World!</h1>
+        </main>
+    );
+}
